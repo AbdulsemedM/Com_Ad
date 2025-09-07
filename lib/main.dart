fb709cd33fe7e650ca53c369dc6909a91ac77ffe
AbdulsemedM <abdumussema22@gamil.com>
2025-09-07 03:11:24 +0300

    chore: remove OneSignal integration and related files
    Remove OneSignal dependency from pubspec.yaml and main.dart
    Delete OneSignalNotificationServiceExtension target and related files
    Update iOS dependencies and version numbers

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
+// import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:upgrader/upgrader.dart';
 import 'app/di/injector.dart';
 import 'app/utils/app_bloc_observer.dart';
@@ -18,11 +18,11 @@ import 'app/utils/app_bloc_observer.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Upgrader.clearSavedSettings();
-  await OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
-  await OneSignal.shared.setAppId('93ea5812-db88-42b8-abc2-010c99e48b56');
-  bool accepted =
-      await OneSignal.shared.promptUserForPushNotificationPermission();
-  print("Accepted Permission: $accepted");
+  // await OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  // await OneSignal.shared.setAppId('93ea5812-db88-42b8-abc2-010c99e48b56');
+  // bool accepted =
+      // await OneSignal.shared.promptUserForPushNotificationPermission();
+  // print("Accepted Permission: $accepted");
   await configureInjection(Environment.prod);
 
   if (Platform.isAndroid) {
