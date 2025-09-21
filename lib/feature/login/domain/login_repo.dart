32e3e1e38dd24ebbe9610c7b5f9596f2fd8f4f77
AbdulsemedM <abdumussema22@gmail.com>
2025-09-21 19:04:10 +0300

    feat(login): add password reset methods to LoginRepo
    Add requestPasswordReset and confirmPasswordReset methods to enable password reset functionality

--- lib/feature/login/domain/login_repo.dart
+++ lib/feature/login/domain/login_repo.dart
@@ -1,3 +1,5 @@
 abstract class LoginRepo {
   Future<String> login(String email, String password);
+  Future<bool> requestPasswordReset(String emailOrPhone);
+  Future<bool> confirmPasswordReset(String emailOrPhone, String token, String newPassword);
 }
